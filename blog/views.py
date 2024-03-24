b300f8d4aaaee1db4f697d2be13a095ed43a93ac
xiaaoo <xz3256@nyu.edu>
2024-03-24 15:40:49 -0700

    finish database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'xiaoyun',
-        'title': 'vayager coffee',
-        'content': 'coffee is good',
-        'date_posted': 'Mar 23, 2024',
-    },
-        {
-        'author': 'yuni',
-        'title': 'Starbucks coffee',
-        'content': 'coffee is okay',
-        'date_posted': 'Mar 22, 2024',
-    },
-        {
-        'author': 'sean',
-        'title': 'cold brew office coffee',
-        'content': 'coffee is great',
-        'date_posted': 'Mar 21, 2024',
-    }
-]
-# this is the logic for how we want to handle when the user goes to blog home page
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'I love Coffee'
     }
     return render(request, 'blog/home.html', context)
